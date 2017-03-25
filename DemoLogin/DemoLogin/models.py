42f1220b4fb6ea6f95a1d53445d539b7ad3a8b16
pratikhit07 <pratik.hit07@gmail.com>
2017-03-25 16:18:22 +0530

    Removed fields created and modified from model Registration

--- DemoLogin/DemoLogin/models.py
+++ DemoLogin/DemoLogin/models.py
@@ -6,9 +6,6 @@ class Registration(models.Model):
     email = models.CharField(unique=True, max_length=30, null=False, blank=False)
     mobile_no = models.CharField(unique=True, max_length=10, null=False, blank=False)
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
     def __unicode__(self):
         return str(self.user.id)+ ' | '+str(self.user.username)
 
